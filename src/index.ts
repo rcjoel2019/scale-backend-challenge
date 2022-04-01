ccd15512265fbab0f86bafb04140a9f541fa0f23
rcjoel2019 <rcjoel2019@gmail.com>
2022-04-01 13:00:45 -0300

    change port

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,6 @@ createConnection().then(async connection => {
     app.listen(3001);
 
 
-    console.log("Express server has started on port 3000. Open http://localhost:3001/users to see results");
+    console.log("Express server has started on port 3001. Open http://localhost:3001/users to see results");
 
 }).catch(error => console.log(error));
